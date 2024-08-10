011a70a2388a322b81a08f859dd16130917c9a38
Smug <zikadika2@gmail.com>
2024-08-10 18:51:11 -0400

    AddedSwapFunctionality and added new scenes

---
+++ Assets/Scripts/Gamesystems/SwapFunctions.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapFunctions : MonoBehaviour
+{
+    public SwapFunctions otherSwap;
+    public SwapFunctions thisSwap;
+    public Transform playerSpawnPos;
+    [Header("Boat")]
+    public WaterBoat waterBoat;
+    public GameObject boatCam;
+    [Header("Player")]
+    public GameObject player;
+    private void Awake()
+    {
+        thisSwap = GetComponent<SwapFunctions>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            Swap();
+        }
+    }
+    private void Swap()
+    {
+        if(tag == "Boat")
+        {
+            otherSwap.enabled = true;
+            boatCam.SetActive(false);
+            waterBoat.enabled = false;
+            player.SetActive(true);
+            player.transform.position = playerSpawnPos.position;
+            player.transform.rotation = playerSpawnPos.rotation;
+            thisSwap.enabled = false;
+        }
+        else if(tag == "Player")
+        {
+            otherSwap.enabled = true;
+            boatCam.SetActive(true);
+            waterBoat.enabled = true;
+            player.SetActive(false);
+            thisSwap.enabled = false;
+        }
+    }
+}
